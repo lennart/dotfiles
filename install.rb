52cc54e302ac4d4c0e4a4613f35cc16e9985ca85
Lennart Melzer <lenni@lenni-lan.lan>
2008-12-21 23:35:32 +0100

    Added Install File again

--- install.rb
+++ install.rb
@@ -1,27 +1,31 @@
 #!/usr/bin/env ruby
 require 'fileutils'
 include FileUtils
+
+
+
+
 File.class_eval do
-  root_path = File::expand_path(File::dirname(__FILE__))
+  root_path = expand_path(dirname(__FILE__))
   files = %w{~/.vim ~/.vimrc ~/.gvimrc ~/.gitconfig}
   existing = lambda do |files| 
-    files.inject(true) do |exists,current|
-      exists or File::exists?(File::expand_path(current) )
+    files.inject(false) do |exists,current|
+      exists or exists?(expand_path(current) )
     end
   end
   files_exists = existing.call files
   puts "fileS: #{files_exists}"
   unless files_exists
-    ln_s File.join(root_path, ".vim"), File.expand_path("~"), :verbose => true
-    rm File.expand_path(".vim-extras"), :verbose => true
-    ln_s File.join(root_path, ".vim-extras"), File.expand_path("~"), :verbose => true
+    ln_s join(root_path, ".vim"), expand_path("~"), :verbose => true
+    rm expand_path(".vim-extras"), :verbose => true
+    ln_s join(root_path, ".vim-extras"), expand_path("~"), :verbose => true
     suffix = "mac"
     if (/^Linux/.match(`uname` ) )
       suffix = "linux"
     end
-    ln_s File.join(root_path, ".vimrc"), File.expand_path("~/.vimrc"), :verbose => true
-    ln_s File.join(root_path, ".gvimrc-#{suffix}"), File.expand_path("~/.gvimrc"), :verbose =>true
-    ln_s File.join(root_path, ".gitconfig"), File.expand_path("~/.gitconfig"), :verbose =>true
+    ln_s join(root_path, ".vimrc"), expand_path("~/.vimrc"), :verbose => true
+    ln_s join(root_path, ".gvimrc-#{suffix}"), expand_path("~/.gvimrc"), :verbose =>true
+    ln_s join(root_path, ".gitconfig"), expand_path("~/.gitconfig"), :verbose =>true
   else
     puts "Cannot proceed Files exists: \n #{files.join("\n")}"
   end
